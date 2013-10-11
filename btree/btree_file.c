2bb6746694aa485926af0fb26edf5f7a8d0c78d2
Sandeep Mann <smann@vmware.com>
2013-10-11 12:28:35 -0700

    more comments

--- btree/btree_file.c
+++ btree/btree_file.c
@@ -1,4 +1,8 @@
-
+/*
+ *----------------------------------------------------------------------------
+ * btree_file.c
+ *----------------------------------------------------------------------------
+ */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -8,12 +12,26 @@
 #include "btree.h"
 #include "debug.h"
 
+/*
+ *----------------------------------------------------------------------------
+ *
+ * BTREE_FileOpen --
+ *
+ *----------------------------------------------------------------------------
+ */
 int BTREE_FileOpen(const char* file, int init) {
    int flags = O_RDWR | O_CREAT | (init ? O_TRUNC : 0);
    return file == NULL ? -EINVAL :
           open(file, flags, S_IRUSR|S_IWUSR);
 }
 
+/*
+ *----------------------------------------------------------------------------
+ *
+ * BTREE_FileWriteNode --
+ *
+ *----------------------------------------------------------------------------
+ */
 int BTREE_FileWriteNode(DEV fd, BTREE_Node *n)
 {
    off_t offset = (BTREE_NODE_SZ * n->index) + BTREE_SUPER_OFFSET;
@@ -22,15 +40,36 @@ int BTREE_FileWriteNode(DEV fd, BTREE_Node *n)
    return write(fd, n, sizeof(*n)) == sizeof(*n);
 }
 
+/*
+ *----------------------------------------------------------------------------
+ *
+ * BTREE_FileReadNode --
+ *
+ *----------------------------------------------------------------------------
+ */
 int BTREE_FileReadNode(DEV fd, BTREE_Node *b) {
    return 0;
 }
 
+/*
+ *----------------------------------------------------------------------------
+ *
+ * BTREE_FileWriteRaw --
+ *
+ *----------------------------------------------------------------------------
+ */
 int BTREE_FileWriteRaw(DEV fd, void *b, size_t sz, off_t off) {
    lseek(fd, off, SEEK_SET);
    return write(fd, b, sz);
 }
 
+/*
+ *----------------------------------------------------------------------------
+ *
+ * BTREE_FileReadRaw --
+ *
+ *----------------------------------------------------------------------------
+ */
 int BTREE_FileReadRaw(DEV fd, void *b, size_t sz, off_t off) {
    lseek(fd, off, SEEK_SET);
    return read(fd, b, sz);
@@ -44,6 +83,13 @@ BTREE_Ops BTREE_FileOps = {
    .ReadRaw    = BTREE_FileReadRaw,
 };
 
+/*
+ *----------------------------------------------------------------------------
+ *
+ * BTREE_FileGetOps --
+ *
+ *----------------------------------------------------------------------------
+ */
 BTREE_Ops* BTREE_FileGetOps(void)
 {
    return &BTREE_FileOps;
